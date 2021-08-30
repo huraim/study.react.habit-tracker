761dadf851f08f5b4d09cdce489c2ba923d7ff33
taka <taka@mewpot.com>
2021-08-30 21:57:31 +0900

    [Study] Navbar 컴포넌트 생성

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
 import Habits from './components/habits';
+import Navbar from './components/navbar';
 
-function App() {
-  return <Habits />; 
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: 'Reading', count: 0 },
+      { id: 2, name: 'Tunning', count: 0 },
+      { id: 3, name: 'Coding', count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter(item => item.id !== habit.id) //클릭한거 와 다른  아이디의 배열을 필터
+    this.setState({ habits }); //업데이트
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar 
+          totalCount={this.state.habits.filter(item => item.count > 0).length}
+        /> 
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -2,30 +2,24 @@ import React, { Component } from 'react';
 import Habit from './habit';
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: 'Reading', count: 0 },
-      { id: 2, name: 'Tunning', count: 0 },
-      { id: 3, name: 'Coding', count: 0 },
-    ],
-  };
-
+  
   handleIncrement = (habit) => {
-    console.log(`handleIncrement ${habit.name}`);
+    this.props.onIncrement(habit);
   };
 
   handleDecrement = (habit) => {
-    console.log(`handleDecrement ${habit.name}`);
+    this.props.onDecrement(habit);
   };
 
   handleDelete = (habit) => {
-    console.log(`handleDelete ${habit.name}`);
+    this.props.onDelete(habit);
   };
+  
 
   render() {
     return (
       <ul>
-        {this.state.habits.map(habit => (
+        {this.props.habits.map(habit => (
             <Habit 
             key={habit.id} 
             habit={habit} 
